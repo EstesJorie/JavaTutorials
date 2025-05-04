602cce7b439033946444cfba4775e1511f17d9bf
EstesJorie <joe.tresise@aol.com>
2025-05-04 23:12:23 +0100

    update to if statement file

--- IfStatements/src/com/estesjorie/Main.java
+++ IfStatements/src/com/estesjorie/Main.java
@@ -2,6 +2,14 @@ package com.estesjorie;
 
 public class Main {
     public static void main(String[] args) {
-        
+        int temp = 32;
+        if (temp > 30){
+            System.out.println("It's a hot day!");
+            System.out.println("Drink lots of water!");
+        } else if (temp > 20 && temp <= 30) {
+            System.out.println("It's a nice day!");
+        } else {
+            System.out.println("Cold day!");
+        }
     }
 }
