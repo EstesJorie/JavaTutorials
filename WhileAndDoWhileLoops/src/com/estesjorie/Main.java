dd9a67b9e628ebf8bc62061c14423bd0573a80fd
EstesJorie <joe.tresise@aol.com>
2025-05-09 19:01:24 +0100

    Addition of WhileAndDoWhileLoops

---
+++ WhileAndDoWhileLoops/src/com/estesjorie/Main.java
@@ -0,0 +1,21 @@
+package com.estesjorie;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String input = "";
+        Scanner scanner = new Scanner(System.in);
+        while (!input.equals("quit")) {
+            System.out.print("Input: ");
+            input = scanner.next().toLowerCase();
+            System.out.println(input);
+        }
+
+        do {
+            System.out.print("Input: ");
+            input = scanner.next().toLowerCase();
+            System.out.println(input);
+        } while (!input.equals("quit"));
+    }
+}
