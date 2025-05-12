2d69d4f15fb25e001adab17c13b78422c3f77821
EstesJorie <joe.tresise@aol.com>
2025-05-12 22:16:20 +0100

    addition of foreach files

---
+++ For-Each Loop/src/com/estesjorie/Main.java
@@ -0,0 +1,11 @@
+package com.estesjorie;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] fruits = {"Apple", "Mango", "Orange"};
+
+        for (String fruit : fruits) { //foreach loop
+            System.out.println(fruit);
+        }
+    }
+}
